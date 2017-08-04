276ea8d3fa97702f9b20d203cc539a8fa08b0417
Hashem Khalifa <hashem.khalifa@tajawal.com>
2017-08-04 18:19:54 +0400

    Add more examples to destructuring

--- destructuring.js
+++ destructuring.js
@@ -11,3 +11,28 @@ var expense = {
 //const { type } = expense;
 //const { amount } = expense;
 const { type, amount } = expense;
+
+
+// Another example.
+
+var userInfo = {
+    firstName: 'Hashem',
+    lastName: 'Khalifa',
+    age: 28
+}
+
+var userGraduate = {
+    university: 'university of sharjah',
+    graduated: '2011'
+}
+
+// var userSummary = function(user) {
+//     return `Welcome ${user.firstName} ${user.lastName} ${user.age}`;
+// }
+
+// destructuring 
+var userSummary = function({ firstName, lastName, age }, { university, graduated }) {
+    return `Welcome ${firstName} ${lastName} ${age}` + ` your ${university} from ${graduated}`;
+}
+
+userSummary(userInfo, userGraduate)
