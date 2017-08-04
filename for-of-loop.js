1062e043875d9eeecd48e6064230a716b120b48f
Hashem Khalifa <hashem.khalifa@tajawal.com>
2017-08-05 00:46:00 +0400

    Add for of loop

---
+++ for-of-loop.js
@@ -0,0 +1,12 @@
+const colors = ['red', 'green', 'blue'];
+
+for (let color of colors) {
+
+}
+
+const numbers = [1, 2, 3, 4];
+
+let total = 0;
+for (let number of numbers) {
+    total += numbers;
+}
