eb51ae23a5882e684f08decbd2ee64d8eecfab7d
Hashem Khalifa <hashem.khalifa@tajawal.com>
2017-08-06 09:34:03 +0400

    comment for fetch

--- Promises and Fetch/Fetch.js
+++ Promises and Fetch/Fetch.js
@@ -5,4 +5,4 @@ url = "https://jsonplaceholder.typicode.com/posts/2222"
 fetch(url)
     //.then(response => response.json) // take the response to real acutal json data from the server.
     .then(respone => console.log(respone))
-    .catch(error => console.log('Error cant featch', error))
+    .catch(error => console.log('Error cant featch', error)) // only display if we have error number less than 300 and the domain it self breaks.
